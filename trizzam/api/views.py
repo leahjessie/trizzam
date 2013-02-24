7efb1bbab71fed4a38e41cb58704ea18639d4ba5
Olivia Bishop <olivia@fb.com>
2013-02-23 22:02:06 -0800

    display planet names from freebase

--- trizzam/api/views.py
+++ trizzam/api/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
 
+from apiclient import discovery
+from apiclient import model
+import json
+
 def hi(request):
-      return HttpResponse("Hello, world.")
+  API_KEY = 'AIzaSyBn6iR4pp-a9upfgtvzGm3ZusFQ_YsHhNc'
+  model.JsonModel.alt_param = ""
+  freebase = discovery.build('freebase', 'v1', developerKey=API_KEY)
+  query = [{'id': None, 'name': None, 'type': '/astronomy/planet'}]
+
+  response = json.loads(freebase.mqlread(query=json.dumps(query)).execute())
+  out = ""
+  for planet in response['result']:
+    out = out + planet['name'] + ", "
+  return HttpResponse(out)
